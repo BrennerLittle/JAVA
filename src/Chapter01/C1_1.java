100352a317fb2421854ad9f624497a7666549816
Brenner Little <>
2019-01-10 09:52:41 -0600

    Java Programing

---
+++ src/Chapter01/C1_1.java
@@ -0,0 +1,22 @@
+package Chapter01;
+
+/**
+ * prints out statement
+ *
+ * @author Brenner Little
+ */
+public class C1_1 {
+
+    /**
+     * Main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+    }
+}
